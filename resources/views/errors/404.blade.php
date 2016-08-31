7a41a92f173982385ad39f2b780923d58a55380d
Michael Coyle <mcoyle74@gmail.com>
2016-08-31 14:57:58 -0500

    Created custom 404 page.

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link href="https://fonts.googleapis.com/css?family=IM+Fell+English" rel="stylesheet">
+	<style type="text/css">
+		body {
+			font-family: 'IM Fell English', serif;
+			text-align: center;
+		}
+		h1 {
+			font-size: 4em;
+		}
+	</style>
+	<title>404 - Page not found</title>
+</head>
+<body>
+	<div>
+		<img src="img/herebedragons.jpg" alt="Here be dragons.">
+		<h1>404</h1>
+		<h2>Sorry, page cannot be found.</h2>
+	</div>
+</body>
+</html>
