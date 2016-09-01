285a84e91f13398fc359725603b1265e5b1dacc7
Michael Coyle <mcoyle74@gmail.com>
2016-09-01 15:16:14 -0500

    Added name attributes and default values to edit profile view inputs.

--- resources/views/users/user.blade.php
+++ resources/views/users/user.blade.php
@@ -21,19 +21,19 @@
 					<form>
 						<div class="form-group" action="/users/user">
 							<label for="name">Name</label>
-							<input type="text" class="form-control" id="name" placeholder="Name">
+							<input type="text" class="form-control" id="name" name="name" value="{{ $user->username }}" placeholder="Name">
 						</div>
 						<div class="form-group">
 							<label for="username">Username</label>
-							<input type="text" class="form-control" id="username" placeholder="Username">
+							<input type="text" class="form-control" id="username" name="username" value="{{ $user->username }}" placeholder="Username">
 						</div>
 						<div class="form-group">
 							<label for="email">Email</label>
-							<input type="email" class="form-control" id="email" placeholder="Email">
+							<input type="email" class="form-control" id="email" name="email" value="{{ $user->email }}" placeholder="Email">
 						</div>
 						<div class="form-group">
 							<label for="password">Password</label>
-							<input type="password" class="form-control" id="password" placeholder="Password">
+							<input type="password" class="form-control" id="password" name="password" placeholder="Password">
 						</div>
 						<button type="submit" class="btn btn-default">Submit</button>
 					</form>
