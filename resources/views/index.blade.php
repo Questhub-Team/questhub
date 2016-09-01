0f731dddf58a8158cb08e9f6efcccc2900e1c71f
Michael Coyle <mcoyle74@gmail.com>
2016-09-01 14:42:09 -0500

    Fixed login modal, centered and added bottom margin.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,28 +13,30 @@
 	<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#login-modal">Login</button>
 	<div class="modal fade" id="login-modal" tabindex="-1" role="dialog" aria-labelledby="login form">
 		<div class="modal-dialog modal-lg" role="document">
-			<div class="modal-content container">
-				<h2>Login to Questhub</h2>
-				<form method="POST" action="{{ action('Auth\AuthController@postLogin')}}">
-					{{ csrf_field() }}
-					<div class="form-group">
-						<label for="email">Email</label>
-						<input type="text"
-							class="form-control"
-							id="login-email"
-							placeholder="Email"
-							name="email">
-					</div>
-					<div class="form-group">
-						<label for="password">Password</label>
-						<input type="password"
-							name="password"
-							class="form-control"
-							id="login-password"
-							placeholder="Password">
-					</div>
-					<button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-log-in"></span> Login</button>
-				</form>
+			<div class="modal-content">
+				<div class="container-fluid">
+					<h2>Login to Questhub</h2>
+					<form method="POST" action="{{ action('Auth\AuthController@postLogin')}}" id="login-modal">
+						{{ csrf_field() }}
+						<div class="form-group">
+							<label for="email">Email</label>
+							<input type="text"
+								class="form-control"
+								id="login-email"
+								placeholder="Email"
+								name="email">
+						</div>
+						<div class="form-group">
+							<label for="password">Password</label>
+							<input type="password"
+								name="password"
+								class="form-control"
+								id="login-password"
+								placeholder="Password">
+						</div>
+						<button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-log-in"></span> Login</button>
+					</form>
+				</div>
 			</div>
 		</div>
 	</div>
