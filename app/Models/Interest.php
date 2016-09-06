1408b63a2aefe3c63c1e0ecc552a104e1d0ae35d
Alex Scott <runhearn@gmail.com>
2016-09-06 10:49:43 -0500

    events now linked to interests

--- app/Models/Interest.php
+++ app/Models/Interest.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Interest extends Model
 {
     protected $table = 'interests';
+
+    public static function findByName($name){
+    	return Interest::Where('name', $name)->first();
+    }
 }
+
--- database/seeds/InterestsSeeder.php
+++ database/seeds/InterestsSeeder.php
@@ -42,4 +42,7 @@ class InterestsSeeder extends Seeder
         	$new_interest->save();
         }
     }
+
+
+    
 }
