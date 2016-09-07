50f8545b64d232284069f94465f18f690454261d
Alan <macbookpro@MacBooks-MacBook-Pro.local>
2016-09-07 15:53:12 -0500

    "changed index via css"

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,7 +12,6 @@
 		<div class="col-md-4">
 			<h4>{{ $responseItem['name'] }}</a></h4>
 			{{ (isset($responseItem['description'])) ? strip_tags($responseItem['description']) : '' }}
-			{{ $responseItem['id'] }}
 		</div>
 			<?php $i++ ?>
 			<?php if ($i==3) { ?>
@@ -49,6 +48,10 @@
 			</div>
 		</div>
 	</div>
-	<a href="{{ action('Auth\AuthController@getRegister')}}" class="btn btn-primary btn-lg">Register</a>
 </div>
+	<div class="container-fluid register-btn">
+		<a href="{{ action('Auth\AuthController@getRegister')}}" class="btn btn-primary btn-lg display-block" id="register">
+			<span class="display-block text-center register-text">Register</span>
+		</a>
+	</div>
 @stop
