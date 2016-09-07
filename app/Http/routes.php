806b5dd0ea3760e71f043f3a83e44c3ab0762a99
Alan <macbookpro@MacBooks-MacBook-Pro.local>
2016-09-07 11:00:40 -0500

    "re did user controller and optimized profile customization"

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,25 +10,21 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-Route::get('auth/login', 'Auth\AuthController@getLogin');
-Route::post('auth/login', 'Auth\AuthController@postLogin');
 Route::resource('/', 'AppController');
-Route::resource('users', 'UsersController');
-Route::resource('apps', 'AppController');
-
-// Registration routes...
-//Route::resource('auth/register', 'UserController@store');
-//Route::resource('auth/register', 'AppController');
-Route::get('auth/register', 'Auth\AuthController@getRegister');
-Route::post('auth/register', 'UsersController@store');
-Route::controllers(['password' => 'Auth\PasswordController',]);
 Route::get('/all-events', 'AppController@showAll');
 
+Route::put('/users/interests', 'UsersController@updateInterests');
+Route::resource('users', 'UsersController');
+
+// Authentication routes...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
 Route::get('auth/logout', 'Auth\AuthController@getLogout');
 
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
 
-Route::put('/', 'UsersController@updateAccount');
-Route::put('/', 'UsersController@updateInterests');
 
 
 
