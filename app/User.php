dc354e8b86213b6b52d4c081bb19b5c8d242909f
Michael Coyle <mcoyle74@gmail.com>
2016-09-08 09:42:23 -0500

    Fiddling with image upload.

--- app/User.php
+++ app/User.php
@@ -54,19 +54,19 @@ class User extends Model implements AuthenticatableContract,
      */
     protected $hidden = ['password', 'remember_token'];
 
-    public static function saveUploadedImage($fileToUpload)
+    public static function saveUploadImage($fileToUpload)
     {
         $valid = true;
 
         // checks if $input_name is in the files super global
-        if(isset($_FILES[$fileToUpload]) && $_FILES[$fileToUpload]['name']) {
+        if(isset($_FILES[$fileToUpload]) && $_FILES[$fileToUpload]['profile_img']) {
 
             // checks if there are any errors on the upload from the submission
             if(!$_FILES[$fileToUpload]['error']) {
 
                 $tempFile = $_FILES[$fileToUpload]['tmp_name'];
-                    $image_ext = pathinfo($_FILES[$fileToUpload]['name'], PATHINFO_EXTENSION);
-                    $image_url = __DIR__ . '/../public/img/uploads' . $tempFile . '.' . $image_ext;
+                    $image_ext = pathinfo($_FILES[$fileToUpload]['profile_img'], PATHINFO_EXTENSION);
+                    $image_url = __DIR__ . '/../public/img' . $tempFile . '.' . $image_ext;
                     $r = move_uploaded_file($tempFile, $image_url);
                     return $image_url;  
             }
