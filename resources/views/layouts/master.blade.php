85350b8b11380d0c46c9c427e91f4bd0af4c0891
Michael Coyle <mcoyle74@gmail.com>
2016-09-08 16:49:01 -0500

    Restored blade @yield('content') to master template

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -23,7 +23,8 @@
 	<body>
 		<div class="container-fluid scroll">
 			@include('layouts.partials.navbar')
-
+			@yield('content')
+		</div>
 
 	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
