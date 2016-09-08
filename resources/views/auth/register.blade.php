cdedf5827d12a311f6e88ba0b48aa2b5b59c587c
Alan <macbookpro@MacBooks-MacBook-Pro.local>
2016-09-08 16:30:22 -0500

    "working on minor bugs in user profile and style"

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -66,7 +66,7 @@
     <div class="form-group">
         <label class="col-md-4 control-label">Confirm Password</label>
         <div class="col-md-6">
-            <input type="password" class="form-control" name="password_confirmation" required>
+            <input type="password" class="form-control" name="password_confirmation" >
         </div>
     </div>
     <div class="form-group">
