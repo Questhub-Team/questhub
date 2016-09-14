35df75cd0d23131c77c0c2e751da0d3ced1282d4
Alan <macbookpro@MacBooks-MacBook-Pro.local>
2016-09-14 11:07:15 -0500

    "changed the search results page to match the style of the rest of the events"

--- resources/views/events/searchresults.blade.php
+++ resources/views/events/searchresults.blade.php
@@ -1,21 +1,12 @@
 @extends('layouts.master')
 @section('content')
-<style type="text/css">
-	.events {
-		height: 400px;
-		width: 250px;
-		overflow-y: auto;
-	}
 </style>
-	<div>
-		<h1 class="fancy-header">Search Result</h1>
+		<div class="col-lg-12 events-container">
 			@foreach ($searchResult as $responseItem)
 				<div class="col-md-4 events">
-					<h4>Name: {{ $responseItem->name }}</h4>
-
-						<p>Location: {{ (isset($responseItem->venue)) ? implode($responseItem->venue, ' ') : '' }}</p>
-
-						<p>{{ (isset($responseItem->description)) ? strip_tags($responseItem->description) : '' }}</p>
+					<h4>Name: {{ $responseItem->name }}</a></h4>
+						<button class="btn btn-default show">Show Description</button>
+						<p style="display: none" id="{{ $responseItem->id }}">{{ (isset($responseItem->description)) ? strip_tags($responseItem->description) : '' }}</p>
 					<form method="POST" action="{{ action('EventsController@store') }}">
 						{{ csrf_field() }}
 						<input type='hidden' name='user_id' value="{{ Auth::user()->id }}">
@@ -24,6 +15,17 @@
 					</form>
 				</div> 
 			@endforeach
+		</div>
 			<span colspan="7" class="text-center">{!! $searchResult->render() !!}</span>
-	</div>
+	<script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+	<script>
+		(function(){
+			$( document ).ready(function() {
+				var id = {{ $responseItem->id }}
+				$('.show').click(function(){
+					$(this).next('p').show('slow');
+				});
+			});	
+		})();
+	</script>
 @stop
